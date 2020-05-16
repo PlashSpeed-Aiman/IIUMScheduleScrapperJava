d0181b68d76dae64be129bda145ac1b6e86334c9
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2020-05-17 02:43:08 +0800

    Some update to make it better

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,35 +6,36 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
+
+    final static List<List<String>> GroupBind = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         String url = "http://albiruni.iium.edu.my/myapps/StudentOnline/schedule1.php";
         int page_view = 50;
         String[] KulliyyahListVal = {
-                "KAHS",
-                "AED",
-                "BRIDG",
-                "CFL",
-                "CCAC",
-                "DENT",
-                "EDUC",
-                "ENGIN",
-                "ECONS",
-                "KICT",
-                "IHART",
-                "IRKHS",
-                "IIBF",
-                "ISTAC",
-                "KLM",
-                "LAWS",
-                "MEDIC",
-                "NURS",
-                "PHARM",
-                "KOS"};
+                "KAHS",//0
+                "AED",//1
+                "BRIDG",//2
+                "CFL",//3
+                "CCAC",//4
+                "DENT",//5
+                "EDUC",//6
+                "ENGIN",//7
+                "ECONS",//8
+                "KICT",//9
+                "IHART",//10
+                "IRKHS",//11
+                "IIBF",//12
+                "ISTAC",//13
+                "KLM",//14
+                "LAWS",//15
+                "MEDIC",//16
+                "NURS",//17
+                "PHARM",//18
+                "KOS"};//19
         int semesterVal = 2;
 
         String sessionVal = "2019/2020";
@@ -69,6 +70,15 @@ public class Main {
     }
 
     public static void WebScrapper(String url) throws IOException {
+        String subjectTitle;
+        String section;
+        String subjectCode;
+        String Lecture1;
+        String Lecturer2 = null;
+        String Lecturer3 = null;
+
+//        ArrayList<String> DataBind = new ArrayList<String>();
+//        ArrayList<ArrayList<String>> GroupBind = new ArrayList<ArrayList<String>>();
         //Tries to connect. Haven't implement the "if it fails" option
 
         Document document = Jsoup.connect(url).userAgent("Google Chrome").timeout(6000).get();
@@ -77,64 +87,78 @@ public class Main {
 //        System.out.println(subjectCodeRowRough.size());
         //For loop eliminates the parts that don't contain the data we need
         for (int i = 3; i < subjectCodeRowRough.size() - 2; i++) {
-
+            //I put here so that it resets the ArrayList for every iteration
+            List<String> DataBind = new ArrayList<>();
             Element test = subjectCodeRowRough
                     .get(i);
 
-            String subjectCode = test
+            subjectCode = test
                     .select("tr:nth-of-type(n) > td:nth-of-type(1)")
                     .first()
                     .text();
 
-            System.out.println("Code: " + subjectCode);
+//            System.out.println("Code: " + subjectCode);
 
-            String subjectTitle = test
+            subjectTitle = test
                     .select("tr:nth-of-type(n) >td:nth-of-type(3)")
                     .first()
                     .text();
-            System.out.println("Title: " + subjectTitle);
+//            System.out.println("Title: " + subjectTitle);
 
-            String section = test
+            section = test
                     .select("tr:nth-of-type(n) >td:nth-of-type(2)")
                     .first()
                     .text();
 
-            System.out.println("Section:" + section);
+//            System.out.println("Section:" + section);
 
-            String Lecture1 = test
+            Lecture1 = test
                     .select(" tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)")
                     .first()
                     .text();
-            System.out.println("Lecture 1: " + Lecture1);
+//            System.out.println("Lecture 1: " + Lecture1);
 
-            String Lecturer2;
-            String Lecturer3;
             //check if the index value exist
 
             if (!test.select(" tr:nth-child(n) > td:nth-child(5) > table > tbody > tr:nth-child(2) > td:nth-child(4)").isEmpty()) {
                 //check if its not empty
                 if (!(test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(1).text() == null)) {
                     Lecturer2 = test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(1).text();
-                    System.out.println("Lecturer 2: " + Lecturer2);
+//                    System.out.println("Lecturer 2: " + Lecturer2);
                 }
-            }else if (!test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr:nth-child(3) > td:nth-child(4)").isEmpty()) {
-                    Lecturer3 = test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(2).text();
-                    System.out.println("Lecturer 3: "+ Lecturer3);
+            } else if (!test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr:nth-child(3) > td:nth-child(4)").isEmpty()) {
+                Lecturer3 = test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(2).text();
+//                    System.out.println("Lecturer 3: "+ Lecturer3);
             }
 
             String CreditHour = test
                     .select("tr:nth-of-type(n) >td:nth-of-type(4)")
                     .first()
                     .text();
+
+            System.out.println("Code: "+ subjectCode+"\n" +"Title: " +subjectTitle +"\n" + "Section: "+section);
+            //Unnecessary but it makes life easier
+            String[] BundleList = {subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3};
             System.out.println("Chr: " + CreditHour);
-            //SubjectBinder puts the data into a data structure.
+            for (String element : BundleList)
+                DataBind.addAll(Collections.singleton(element));
+            GroupBind.add(DataBind);
+//            System.out.println(GroupBind);
+
+//            DataBind.clear();
+
+
+//            System.out.println(GroupBind);
+
+
+//              Binder(subjectTitle, section, subjectCode, Lecture1, Lecturer2,  Lecturer3);
 
         }
     }
 
-    public static void putthedamnthinghere(String one, String two, String three) {
-        SubjectBinder subjectBinder = new SubjectBinder(one, two, three);
-
+    public static void Binder(String subjectTitle, String section, String subjectCode, String Lecture1, String Lecturer2, String Lecturer3) {
+        final SubjectBinder subjectBinder = new SubjectBinder();
+        subjectBinder.DataBinder(subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3);
     }
 
 
