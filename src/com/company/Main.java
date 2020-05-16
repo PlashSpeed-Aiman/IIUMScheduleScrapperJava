43a6381bc8baeb024b12c63a0179c3014f12b1f6
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2020-05-16 15:43:02 +0800

    Some update to make it better

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,52 +6,135 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String url = "http://albiruni.iium.edu.my/myapps/StudentOnline/schedule1.php?";
+        String url = "http://albiruni.iium.edu.my/myapps/StudentOnline/schedule1.php";
         int page_view = 50;
-        String kulliyyahStr = "CFL";
+        String[] KulliyyahListVal = {
+                "KAHS",
+                "AED",
+                "BRIDG",
+                "CFL",
+                "CCAC",
+                "DENT",
+                "EDUC",
+                "ENGIN",
+                "ECONS",
+                "KICT",
+                "IHART",
+                "IRKHS",
+                "IIBF",
+                "ISTAC",
+                "KLM",
+                "LAWS",
+                "MEDIC",
+                "NURS",
+                "PHARM",
+                "KOS"};
         int semesterVal = 2;
+
         String sessionVal = "2019/2020";
-        String url2 = "action=view&view=".concat(String.valueOf(page_view)) + "&kuly=".concat(kulliyyahStr) + "&ctype=%3C&course=&sem=".concat(String.valueOf(semesterVal)) + "&ses=".concat(sessionVal);
+
+        String url2 = "?action=view&view=".concat(String.valueOf(page_view)) + "&kuly=".concat(KulliyyahListVal[7]) + "&ctype=%3C&course=&sem=".concat(String.valueOf(semesterVal)) + "&ses=".concat(sessionVal);
+
         String url3 = url + url2;
+//
+//        GetKulyList(url);
+
         WebScrapper(url3);
 
     }
 
+    public static void GetKulyList(String url) throws IOException {
+        Document doc = Jsoup
+                .connect(url)
+                .userAgent("Google Chrome")
+                .timeout(6000).get();
+
+        Elements elements = doc
+                .select("body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > select > option:nth-child(n)");
+
+        for (Element element : elements) {
+            char[] ListKul;
+            ListKul = element
+                    .select("option:nth-child(n)")
+                    .val()
+                    .toCharArray();
+//            System.out.println(ListKul);
+        }
+    }
+
     public static void WebScrapper(String url) throws IOException {
         //Tries to connect. Haven't implement the "if it fails" option
+
         Document document = Jsoup.connect(url).userAgent("Google Chrome").timeout(6000).get();
+
         Elements subjectCodeRowRough = document.select("body > table:nth-child(4) > tbody > tr:nth-child(n)");//Select the table that contains the Data List
-        System.out.println(subjectCodeRowRough.size());
+//        System.out.println(subjectCodeRowRough.size());
         //For loop eliminates the parts that don't contain the data we need
         for (int i = 3; i < subjectCodeRowRough.size() - 2; i++) {
-            Element test = subjectCodeRowRough.get(i);
-            String subjectCode = test.select("tr:nth-of-type(n) > td:nth-of-type(1)").first().text();
+
+            Element test = subjectCodeRowRough
+                    .get(i);
+
+            String subjectCode = test
+                    .select("tr:nth-of-type(n) > td:nth-of-type(1)")
+                    .first()
+                    .text();
+
             System.out.println("Code: " + subjectCode);
-            String subjectTitle = test.select("tr:nth-of-type(n) >td:nth-of-type(3)").first().text();
+
+            String subjectTitle = test
+                    .select("tr:nth-of-type(n) >td:nth-of-type(3)")
+                    .first()
+                    .text();
             System.out.println("Title: " + subjectTitle);
-            String Section = test.select("tr:nth-of-type(n) >td:nth-of-type(2)").first().text();
-            System.out.println("Section:" + Section);
-            String Lecture1 = test.select(" tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").first().text();
+
+            String section = test
+                    .select("tr:nth-of-type(n) >td:nth-of-type(2)")
+                    .first()
+                    .text();
+
+            System.out.println("Section:" + section);
+
+            String Lecture1 = test
+                    .select(" tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)")
+                    .first()
+                    .text();
             System.out.println("Lecture 1: " + Lecture1);
-            String Lecturer2 ;
+
+            String Lecturer2;
+            String Lecturer3;
             //check if the index value exist
+
             if (!test.select(" tr:nth-child(n) > td:nth-child(5) > table > tbody > tr:nth-child(2) > td:nth-child(4)").isEmpty()) {
                 //check if its not empty
-                if (!(test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(1) == null)) {
+                if (!(test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(1).text() == null)) {
                     Lecturer2 = test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(1).text();
                     System.out.println("Lecturer 2: " + Lecturer2);
                 }
-            } else {
-                continue;
+            }else if (!test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr:nth-child(3) > td:nth-child(4)").isEmpty()) {
+                    Lecturer3 = test.select("tr:nth-child(n) > td:nth-child(5) > table > tbody > tr > td:nth-child(4)").get(2).text();
+                    System.out.println("Lecturer 3: "+ Lecturer3);
             }
-            String CreditHour;
+
+            String CreditHour = test
+                    .select("tr:nth-of-type(n) >td:nth-of-type(4)")
+                    .first()
+                    .text();
+            System.out.println("Chr: " + CreditHour);
             //SubjectBinder puts the data into a data structure.
-//                SubjectBinder subjectBinder = new SubjectBinder(subjectCode, Section, subjectTitle);
+
         }
+    }
+
+    public static void putthedamnthinghere(String one, String two, String three) {
+        SubjectBinder subjectBinder = new SubjectBinder(one, two, three);
+
     }
 
 
--- src/com/company/SubjectBinder.java
+++ src/com/company/SubjectBinder.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubjectBinder {
 
     private String Code,Sect,Title,Chr,Day,Time,Venue,Lecturer1,Lecturer2;
-
+    String[] attributesList  = {Code, Sect, Title, Chr, Day, Time, Venue, Lecturer1, Lecturer2};
+    List<String[]> subject = new ArrayList<>();
     public SubjectBinder(String one,String two,String three,String four,String five,String six,String seven,String eight,String nine){
         Code = one;
         Sect = two;
@@ -18,9 +22,16 @@ public class SubjectBinder {
     }
 
     public SubjectBinder(String code, String sect, String title) {
-        Code = code;
-        Sect = sect;
-        Title = title;
+            Code = code;
+            Sect = sect;
+            Title = title;
+            subject.add(attributesList);
         System.out.println(Code+ " " + Sect + " " + Title + " ");
+        test();
+    }
+    public void test(){
+        System.out.println(subject);
+
     }
 }
+
