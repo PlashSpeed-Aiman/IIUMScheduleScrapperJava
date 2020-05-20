aa466a9ac38f4b1f9072763d6888e95d4c865a97
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2020-05-21 07:53:17 +0800

    Page number scraping. Allows for exception catching.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,43 +11,51 @@ import java.util.*;
 public class Main {
 
     final static List<List<String>> GroupBind = new ArrayList<>();
+    static String[] KulliyyahListVal = {
+            "KAHS",//0
+            "AED",//1
+            "BRIDG",//2
+            "CFL",//3
+            "CCAC",//4
+            "DENT",//5
+            "EDUC",//6
+            "ENGIN",//7
+            "ECONS",//8
+            "KICT",//9
+            "IHART",//10
+            "IRKHS",//11
+            "IIBF",//12
+            "ISTAC",//13
+            "KLM",//14
+            "LAWS",//15
+            "MEDIC",//16
+            "NURS",//17
+            "PHARM",//18
+            "KOS"};//19
 
     public static void main(String[] args) throws IOException {
+        URLManipFunc();
+    }
+
+    public static void URLManipFunc() throws IOException {
         String url = "http://albiruni.iium.edu.my/myapps/StudentOnline/schedule1.php";
-        int page_view = 50;
-        String[] KulliyyahListVal = {
-                "KAHS",//0
-                "AED",//1
-                "BRIDG",//2
-                "CFL",//3
-                "CCAC",//4
-                "DENT",//5
-                "EDUC",//6
-                "ENGIN",//7
-                "ECONS",//8
-                "KICT",//9
-                "IHART",//10
-                "IRKHS",//11
-                "IIBF",//12
-                "ISTAC",//13
-                "KLM",//14
-                "LAWS",//15
-                "MEDIC",//16
-                "NURS",//17
-                "PHARM",//18
-                "KOS"};//19
-        int semesterVal = 2;
-
+        int page_view;
+        int semesterVal;
         String sessionVal = "2019/2020";
-
-        String url2 = "?action=view&view=".concat(String.valueOf(page_view)) + "&kuly=".concat(KulliyyahListVal[7]) + "&ctype=%3C&course=&sem=".concat(String.valueOf(semesterVal)) + "&ses=".concat(sessionVal);
-
+        int kulliyyahChoice = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select Kulliyyah");
+        kulliyyahChoice = scanner.nextInt();
+        System.out.println("Select Semester");
+        semesterVal = scanner.nextInt();
+        System.out.println("Select Page");
+        page_view = scanner.nextInt();
+        String url2 = "?action=view&view=".concat(String.valueOf(page_view * 50)) + "&kuly=".concat(KulliyyahListVal[kulliyyahChoice]) + "&ctype=%3C&course=&sem=".concat(String.valueOf(semesterVal)) + "&ses=".concat(sessionVal);
         String url3 = url + url2;
-//
+        //Only used once coz I'm too lazy to write the values one by one
 //        GetKulyList(url);
 
         WebScrapper(url3);
-
     }
 
     public static void GetKulyList(String url) throws IOException {
@@ -76,12 +84,41 @@ public class Main {
         String Lecture1;
         String Lecturer2 = null;
         String Lecturer3 = null;
+        String pageNumberVal = null;
 
 //        ArrayList<String> DataBind = new ArrayList<String>();
 //        ArrayList<ArrayList<String>> GroupBind = new ArrayList<ArrayList<String>>();
         //Tries to connect. Haven't implement the "if it fails" option
 
         Document document = Jsoup.connect(url).userAgent("Google Chrome").timeout(6000).get();
+        Elements pageNumberRough = document.select("body > table:nth-child(4) > tbody > tr:nth-child(1) > td > a:nth-last-child(1)");
+        Element pageNumber;
+        try
+        {
+            pageNumber = pageNumberRough.get(0);
+            System.out.println(pageNumber.text());
+            pageNumberVal = pageNumber.text();
+            if (!(pageNumberVal.contains("NEXT")  || pageNumberVal == "next")) {
+                System.out.println(pageNumberVal.concat("URL"));
+            } else {
+                pageNumberRough = document.select("body > table:nth-child(4) > tbody > tr:nth-child(1) > td > a:nth-last-child(2)");
+                pageNumber = pageNumberRough.get(0);
+                pageNumberVal = pageNumber.getAllElements().text();
+                System.out.println(pageNumberVal + "2");
+            }
+        }catch (Exception e){
+            pageNumberVal = "0";
+        }
+//        System.out.println(pageNumber.text());
+//        pageNumberVal = pageNumber.text();
+//        if (!(pageNumberVal.contains("NEXT")  || pageNumberVal == "next")) {
+//            System.out.println(pageNumberVal.concat("URL"));
+//        } else {
+//            pageNumberRough = document.select("body > table:nth-child(4) > tbody > tr:nth-child(1) > td > a:nth-last-child(2)");
+//            pageNumber = pageNumberRough.get(0);
+//            pageNumberVal = pageNumber.getAllElements().text();
+//            System.out.println(pageNumberVal + "2");
+//        }
 
         Elements subjectCodeRowRough = document.select("body > table:nth-child(4) > tbody > tr:nth-child(n)");//Select the table that contains the Data List
 //        System.out.println(subjectCodeRowRough.size());
@@ -136,14 +173,15 @@ public class Main {
                     .first()
                     .text();
 
-            System.out.println("Code: "+ subjectCode+"\n" +"Title: " +subjectTitle +"\n" + "Section: "+section);
+            System.out.println("Code: " + subjectCode + "\n" + "Title: " + subjectTitle + "\n" + "Section: " + section);
             //Unnecessary but it makes life easier
-            String[] BundleList = {subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3};
+            String[] BundleList = {subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3, pageNumberVal};
             System.out.println("Chr: " + CreditHour);
             for (String element : BundleList)
                 DataBind.addAll(Collections.singleton(element));
             GroupBind.add(DataBind);
-//            System.out.println(GroupBind);
+
+            System.out.println(GroupBind);
 
 //            DataBind.clear();
 
@@ -154,12 +192,16 @@ public class Main {
 //              Binder(subjectTitle, section, subjectCode, Lecture1, Lecturer2,  Lecturer3);
 
         }
+
     }
-
-    public static void Binder(String subjectTitle, String section, String subjectCode, String Lecture1, String Lecturer2, String Lecturer3) {
-        final SubjectBinder subjectBinder = new SubjectBinder();
-        subjectBinder.DataBinder(subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3);
-    }
-
-
 }
+
+//    public static void Binder(String subjectTitle, String section, String subjectCode, String Lecture1, String Lecturer2, String Lecturer3) {
+//        final SubjectBinder subjectBinder = new SubjectBinder();
+//        subjectBinder.DataBinder(subjectTitle, section, subjectCode, Lecture1, Lecturer2, Lecturer3);
+//    }
+
+
+
+
+
